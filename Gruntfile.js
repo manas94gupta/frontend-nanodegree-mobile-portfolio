43b83fa122497b39837f21800c93516c931c5535
Manas <manas94gupta@gmail.com>
2016-11-02 05:33:47 +0530

    Minify scripts
    Minify scripts using uglify grunt plug-in

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,14 +13,25 @@ module.exports = function(grunt) {
                     dest: 'dist/img/'
                 }]
             }
+        },
+        uglify: {
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: 'js/',
+                    src: ['**/*.js'],
+                    dest: 'dist/js/'
+                }]
+            }
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['imagemin']);
+    grunt.registerTask('default', ['imagemin', 'uglify']);
 
 };
